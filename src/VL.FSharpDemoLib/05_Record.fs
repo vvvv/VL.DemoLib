efbb994eadb33eba70fb1920b2cf0334a7f673dd
tebjan <tebjan@vvvv.org>
2017-08-08 15:51:12 +0200

    fixed file order and minor cosmetics

--- src/VL.FSharpDemoLib/05_Record.fs
+++ src/VL.FSharpDemoLib/05_Record.fs
@@ -3,8 +3,9 @@
 //F# types are immutable by default, 
 //so every type that does not contain the keyword 'mutable' can be imported as VL record
 
-//the most easy example is the F#'s own record type
+//the most easy example is F#'s own record type.
 //we can even attach some helper methods to "modify" instances of the record
+//using the 'with' keyword
 type MyFSharpRecord = { X:float32; Y:float32; Name:string }
     with //helper methods
     member this.SetX x = { this with X=x }
