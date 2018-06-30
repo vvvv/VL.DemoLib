7a32bd03fef905a9e87499186129d23a58f2a221
gregsn <gregor@vvvv.org>
2018-06-30 13:43:24 +0200

    added a window that saves its bounds into a process node pin

---
+++ src/VL.DemoLib/MyWindow.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace DemoLib
+{
+    partial class MyWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MyWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 502);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "MyWindow";
+            this.Text = "MyWindow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ src/VL.DemoLib/MyWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Windows.Forms;
+using VL.Core.Properties;
+using VL.Lib.IO;
+
+namespace DemoLib
+{
+    public partial class MyWindow : Form, IDisposable
+    {
+        public Subject<Rectangle> BoundsChanged { get; }
+
+        public void SetSize(Rectangle bounds)
+        {
+            var boundsinPix = Settings.DIPToPixel(bounds);
+            if (boundsinPix != Bounds)
+                Bounds = boundsinPix;
+        }
+
+        public MyWindow()
+        {
+            BoundsChanged = new Subject<Rectangle>();
+            InitializeComponent();
+            SetSize(new Rectangle(1200, 50, 600, 400));
+            Show();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            BoundsChanged.OnNext(Settings.DIP(Bounds));
+        }
+
+        protected override void OnLocationChanged(EventArgs e)
+        {
+            base.OnLocationChanged(e);
+            BoundsChanged.OnNext(Settings.DIP(Bounds));
+        }
+    }
+}
